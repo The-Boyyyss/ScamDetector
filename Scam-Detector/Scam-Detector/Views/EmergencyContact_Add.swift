64ad783a58b5ef57148a9a58609cce0eb8ccf43d
Matt Caballero <77181965+matthlry@users.noreply.github.com>
2022-05-24 22:01:14 -0700

    Added emergency contact page files

---
+++ Scam-Detector/Scam-Detector/Views/EmergencyContact_Add.swift
@@ -0,0 +1,95 @@
+//
+//  EmergencyContact_Add.swift
+//  Scam-Detector
+//
+//  Created by Matt Hilarry Caballero on 2022-05-23.
+//
+
+import SwiftUI
+import Combine
+
+struct EmergencyContact_Add: View {
+    @Binding var name: String;
+    @Binding var number: String;
+    @Binding var isFinished: Bool;
+    
+    @State var inputNumber: String = "";
+    @State var inputName: String = "";
+    @State var showAlert: Bool = false;
+    
+    var body: some View {
+        ZStack(alignment: .top){
+            Color(red: 192 / 255, green: 223 / 255, blue: 161 / 255)
+                                .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .center, spacing: 0){
+                HStack(alignment: .top){
+                    Image(systemName: "phone.fill")
+                        .padding(.trailing, UIScreen.main.bounds.width*0.05)
+                        .padding(.top, UIScreen.main.bounds.height*0.02)
+                    Text("Emergency Contact")
+                }
+                .foregroundColor(Color(red: 1 / 255, green: 25 / 255, blue: 54 / 255))
+                .font(.system(size: UIScreen.main.bounds.width*0.08, weight: .bold))
+                .padding(.bottom, UIScreen.main.bounds.height*0.04)
+                .padding(.top, -UIScreen.main.bounds.height*0.05)
+                Spacer().frame(height: 50)
+                                
+                Text("Contact Name")
+                    .font(.system(size: 25, weight: .heavy))
+                TextField("\(name)", text: $inputName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .font(.system(size: 20))
+                    .onAppear{
+                        inputName = name;
+                    }
+                
+                Text("Phone Number")
+                    .font(.system(size: 25, weight: .heavy))
+                    .padding(.top, UIScreen.main.bounds.height * 0.05)
+                TextField("\(number)", text: $inputNumber)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .font(.system(size: 20))
+                    .keyboardType(.decimalPad)
+                    .onReceive(Just(inputNumber)) {
+                        newValue in let filtered = newValue.filter { "0123456789".contains($0) }
+                        if filtered != newValue {
+                            inputNumber = filtered;
+                        }
+                        if inputNumber.count > 10 {
+                            inputNumber = String(inputNumber.prefix(10))
+                        }
+                    }
+                    .onAppear{
+                        inputNumber = number;
+                    }
+                Spacer().frame(height: 50)
+                
+                Button("Submit"){
+                    // Uncomment for empty textfield check
+                    // if inputName.isEmpty || inputNumber.isEmpty {
+                        // showAlert = true;
+                        // return;
+                    // }
+                    name = inputName;
+                    number = inputNumber;
+                    isFinished = false;
+                }
+                .frame(width: 270, height: 80, alignment: .center)
+                .background(Color(red: 1 / 255, green: 25 / 255, blue: 54 / 255))
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .font(.system(size: 30))
+            }
+            .frame(width: UIScreen.main.bounds.width * 0.85, height: UIScreen.main.bounds.height * 0.75, alignment: .top)
+        }
+        .alert("Error", isPresented: $showAlert, actions: {
+            Button("Try Again", role: .cancel, action: {})
+        }, message: {
+            Text("Please Fill All Fields")
+        })
+    }
+}
+
+// REFERENCE
+// Numbers only in textfield https://stackoverflow.com/questions/58733003/how-to-create-textfield-that-only-accepts-numbers/58736068#58736068
+
