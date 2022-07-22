e725687a12dc985eea4ac863bddeec152bac7631
Matt Caballero <matthlry@hotmail.com>
2022-07-21 23:03:37 -0700

    fixed spacing on keyboard dismiss function

--- Scam-Detector/Scam-Detector/Utilities/KeyboardDismiss.swift
+++ Scam-Detector/Scam-Detector/Utilities/KeyboardDismiss.swift
@@ -9,8 +9,7 @@ import Foundation
 import SwiftUI
 
 func KeyboardDismiss(){
-    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder),
-                                            to: nil, from: nil, for: nil)
+    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
 }
 
 // Dismiss keyboard https://stackoverflow.com/questions/60013190/textfield-swiftui-dismiss-keyboard
